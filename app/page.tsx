439848b556eaf5fe801bd536f53bbc98ff9f12bb
jospen-kah <ngumkahjospen@gmail.com>
2025-03-14 13:01:16 +0100

    home-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,69 @@
 import Image from "next/image";
+import Form from 'next/form';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="px-2 flex flex-col items-center  min-h-screen 
+    py-2 bg-[url('/background-mobile.png')] md:bg-[url('/background-tablet.png')] 
+    lg:bg-[url('/background-desktop.png')] bg-cover bg-center">
+      <div className="flex flex-col gap-1 items-center ">
+        <div>
+          <Image src="/logo-full.svg" alt="logo" width={100} height={100} className="py-4" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <h2 className="text-white md:text-2xl  lg:text-3xl py-2">Your Journey to coding conf 2025 starts here!</h2>
+        <p className="text-white">Secure your spot at next year biggest coding conf. </p>
+      </div>
+      <div className="flex flex-col justify-center ">
+        <label htmlFor="image-upload" className="text-white py-2" >
+          Upload Avatar</label>
+        <div className="flex items-center justify-center md:w-100">
+          <label
+            htmlFor="image"
+            className="flex flex-col items-center justify-center w-full  border-2 border-dashed border-gray-400 px-20 rounded-lg cursor-pointer "
+          >
+            <div className="flex flex-col items-center justify-center pt-3 pb-2">
+
+              <Image src="/icon-upload.svg" alt="upload" width={50} height={50} className="border-2 border-gray-400 p-1 rounded-md" />
+
+              <p className="mb-2 text-sm text-white py-2">
+                <span className="font-semibold ">Drag and drop</span> or click to upload
+              </p>
+            </div>
+            <input id="image" type="file" className="hidden" accept="image/*" />
+          </label>
+        </div>
+        <div className="flex items-center gap-2 py-2">
+          <Image src="/icon-info.svg" alt="check" width={20} height={20} />
+          <p className="text-xs text-gray-400">Upload your photo(JPG or PNG ,max 500KB)</p>
+        </div>
+      <Form action="/search" >
+        <div className="flex flex-col gap-1">
+          <label htmlFor="name" className="text-white py-2">Full Name
+          </label>
+        <input name="username" className="border-2 p-2 border-gray-400 rounded-md" />
+        </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="name" className="text-white py-2">Email Address
+          </label>
+        <input name="email" className="border-2 p-2 border-gray-400 rounded-md" />
+        </div>
+
+        <div className="flex flex-col gap-1 pb-4">
+          <label htmlFor="name" className="text-white py-2">Github Username
+          </label>
+        <input name="githubname" className="border-2 p-2  border-gray-400 rounded-md" />
+        </div>
+        
+        <button type="submit" className="bg-orange-400 py-2 w-full rounded-md flex justify-center items-center">Generate My Ticket </button>
+      </Form>
+
+      </div>
+
+      <div className="border-2 border-white">
+        <p className="text-white">Hello </p>
+        <Image src="/pattern-lines.svg" alt="scroll" width={20} height={20} />
+      </div>
     </div>
   );
 }
